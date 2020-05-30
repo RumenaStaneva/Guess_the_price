c4a302d149ec8d5571749022256ef79fe821a366
Rumena Staneva <63191096+RumenaStaneva@users.noreply.github.com>
2020-05-30 10:33:10 +0300

    first commit

---
+++ Makeup-quiz/app.js
@@ -0,0 +1,31 @@
+const userInput = document.getElementById('input');
+const btn = document.getElementById('button-submit');
+
+function getUserInput(){
+    return parseInt(userInput.value)
+}
+
+function trueOrfalse(){
+    let enteredPrice = getUserInput()
+    if (products.makeup.jamesPalette.price == enteredPrice) {
+        alert("Correct! Bravo");
+    } else {
+        alert("Wrong")
+    }
+}
+
+const makeup = products.makeup;
+
+
+/* const products = {
+    makeup: {
+        jamesPalette: {
+            name: "James Palette",
+            price: 49
+        }
+    },
+    shoes: {}
+} */
+
+btn.addEventListener('click', trueOrfalse);
+

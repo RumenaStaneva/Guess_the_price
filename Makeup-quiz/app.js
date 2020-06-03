04ab671d30e1fd5ec55840251ecccb7c1f1142e3
Rumena Staneva <63191096+RumenaStaneva@users.noreply.github.com>
2020-06-03 12:59:03 +0300

    third commit

--- Makeup-quiz/app.js
+++ Makeup-quiz/app.js
@@ -1,31 +1,50 @@
 const userInput = document.getElementById('input');
 const btn = document.getElementById('button-submit');
 
+
+let rightAnswer = false;
+
 function getUserInput(){
     return parseInt(userInput.value)
 }
 
 function trueOrfalse(){
     let enteredPrice = getUserInput()
+    
     if (products.makeup.jamesPalette.price == enteredPrice) {
         alert("Correct! Bravo");
+        rightAnswer = true;
+        nextPic()
     } else {
         alert("Wrong")
     }
+    document.getElementById('input').value = '';
+}
+function nextPic(){
+    
+    if(rightAnswer == true){
+    document.getElementById("picture").src = 'norvina.png';
+    document.getElementById("namePalette").innerHTML = 'Norvina ABH palette';
+} 
 }
 
-const makeup = products.makeup;
+//const makeup = products.makeup;
 
 //nikola's guidance <3
-/* const products = {
+const products = {
     makeup: {
         jamesPalette: {
             name: "James Palette",
             price: 49
+        },
+        norvinaPalette: {
+        name: 'Norvina',
+        price: 42
         }
     },
     shoes: {}
-} */
+} 
 
 btn.addEventListener('click', trueOrfalse);
+
 

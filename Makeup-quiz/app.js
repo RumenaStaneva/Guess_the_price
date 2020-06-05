08349791fac5da28eac58f026625ee6346ce0e17
Rumena Staneva <63191096+RumenaStaneva@users.noreply.github.com>
2020-06-05 13:02:17 +0300

    working right and wrong

--- Makeup-quiz/app.js
+++ Makeup-quiz/app.js
@@ -3,30 +3,51 @@ const btn = document.getElementById('button-submit');
 const closebtn = document.getElementsByClassName("close");
 
 let rightAnswer = false;
+let namePalette = '';
 
 function getUserInput(){
     return parseInt(userInput.value)
 }
 
-document.getElementById("input").addEventListener("keyup", function(pushed) {
-  if (pushed.keyCode === 13) {
-    pushed.preventDefault();
-   document.getElementById("button-submit").click();
-  }
-});
+//edit here to take the name
+/* function getNamePalette() {
+    namePalette = document.getElementById("namePalette").textContent;
+    if(namePalette === 'The James Charles Eyeshadow Palette'){
+        namePalette = 'jamesPalette';
+    } 
+} */
 
+
+//and here 
 function trueOrfalse(){
     let enteredPrice = getUserInput()
-    
+    /*let name = getNamePalette();
+
+    for(property in products.makeup){
+        name = property
+        if(name == namePalette){
+        console.log('Correct!');
+        break;
+        } else {
+            console.log('Wrong!');
+            
+        }
+        
+        
+    } */
     if (products.makeup.jamesPalette.price == enteredPrice) {
         document.getElementById('correct').style.display='block';
         rightAnswer = true;
         nextPic()
     } else {
-        alert("Wrong")
+        document.getElementById('wrong').style.display='block';
+        
     }
+    
     document.getElementById('input').value = '';
 }
+
+//make it random
 function nextPic(){
     
     if(rightAnswer == true){
@@ -34,11 +55,9 @@ function nextPic(){
     document.getElementById("namePalette").innerHTML = 'Norvina ABH palette';
 } 
 }
-
+//fix it, not working
 function pop(){
-    
     document.getElementById('correct').style.display='block';
-    
 }
 
 //const makeup = products.makeup;
@@ -47,12 +66,12 @@ function pop(){
 const products = {
     makeup: {
         jamesPalette: {
-            name: "James Palette",
+            name: "The James Charles Eyeshadow Palette",
             price: 49
         },
         norvinaPalette: {
-        name: 'Norvina',
-        price: 42
+            name: 'Norvina',
+            price: 42
         }
     },
     shoes: {}
@@ -65,6 +84,16 @@ for (i = 0; i < closebtn.length; i++) {
     this.parentElement.style.display = 'none';
   });
 }
+//when pushed enter
+document.getElementById("input").addEventListener("keyup", function(pushed) {
+    if (pushed.keyCode === 13) {
+      pushed.preventDefault();
+     document.getElementById("button-submit").click();
+    }
+  });
+
+
+//when clicked ouside 
 
 
 btn.addEventListener('click', trueOrfalse);

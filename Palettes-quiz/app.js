2443951a7f462bd9a8bdc3442626a8dc4756d9aa
Rumena Staneva <63191096+RumenaStaneva@users.noreply.github.com>
2020-06-13 12:04:09 +0300

    new palettes added

--- Palettes-quiz/app.js
+++ Palettes-quiz/app.js
@@ -26,6 +26,127 @@ const products = {
             image: "pictures/huda.png", 
             id: 2
         }, 
+        {
+            name: "Jeffree Star - Blood Sugar palette",
+            price: 52,
+            image: "pictures/bloodSugar.png", 
+            id: 3
+        },
+        {
+            name: "Jeffree Star - Jawbreaker Palette",
+            price: 58,
+            image: "pictures/jawBreaker.png", 
+            id: 4
+        }, 
+        {
+            name: "Fenty Beauty - Moroccan Spice Eyeshadow Palette",
+            price: 59,
+            image: "pictures/fentyBeauty.png", 
+            id: 5
+        }, 
+        {
+            name: "Huda Beauty - Rose Gold Remastered Palette",
+            price: 56,
+            image: "pictures/hudaBeautyRose.png", 
+            id: 6
+        },
+        {
+            name: "Huda Beauty - Desert Dusk Palette",
+            price: 56,
+            image: "pictures/hudaDesertDusk.png", 
+            id: 7
+        },
+        {
+            name: "Morphe X Jaclyn Hill Eyeshadow Palette",
+            price: 16,
+            image: "pictures/morpheXJacklynHillSmall.png", 
+            id: 8
+        },
+        {
+            name: "Huda Beauty - Mercury Retrograde Palette",
+            price: 58,
+            image: "pictures/hudaMercury.png", 
+            id: 9
+        },
+        {
+            name: "Morphe - The Jaclyn Hill Eyeshadow Palette",
+            price: 39,
+            image: "pictures/jacklinHillMorphe.png", 
+            id: 10
+        },
+        {
+            name: "Anastasia Beverly Hills - Riviera Eye Shadow Palette",
+            price: 46,
+            image: "pictures/jacklinHillMorphe.png", 
+            id: 11
+        },
+        {
+            name: "Anastasia Beverly Hills - Soft Glam Eye Shadow Palette",
+            price: 46,
+            image: "pictures/jacklinHillMorphe.png", 
+            id: 12
+        },
+        {
+            name: "ICONIC London - Loose Pigment Palette",
+            price: 45,
+            image: "pictures/iconicLondon.png", 
+            id: 13
+        },
+        {
+            name: "Lime Crime - Venus XL Palette",
+            price: 56,
+            image: "pictures/limeCrime.png", 
+            id: 14
+        },
+        {
+            name: "Urban Decay - Naked Ultra Violet Palette",
+            price: 43,
+            image: "pictures/urbanDecayViolet.png", 
+            id: 15
+        },
+        {
+            name: "Urban Decay - Naked Heat Eyeshadow Palette",
+            price: 43,
+            image: "pictures/urbanDecayHeat.png", 
+            id: 16
+        },
+        {
+            name: "Urban Decay - Born To Run Eyeshadow Palette",
+            price: 43,
+            image: "pictures/urbanDecayBornToHeat.png", 
+            id: 17
+        },
+        {
+            name: "BEAUTY BAY - Bright 42 colour palette",
+            price: 23,
+            image: "pictures/beautyBayPalette.png", 
+            id: 18
+        },
+        {
+            name: "Juvia's Place - Zuzu eyeshadow palette",
+            price: 23,
+            image: "pictures/juviasPlaceZuzu.png", 
+            id: 19
+        },
+        {
+            name: "Anastasia Beverly Hills - Amrezy eyeshadow palette",
+            price: 54,
+            image: "pictures/ABHAmrezy.png", 
+            id: 20
+        },
+        {
+            name: "BPerfect - Stacey Marie Carnival palette",
+            price: 48,
+            image: "pictures/carnival.png", 
+            id: 21
+        },
+        {
+            name: "Jeffree Star - Conspiracy Palette",
+            price: 52,
+            image: "pictures/jsConspiracy.png", 
+            id: 22
+        },
+
 
     ],
     shoes: {}

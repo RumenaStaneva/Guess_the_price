f9448cd377b34348ad8f45289a18ee5ab50e08ee
Rumena Staneva <63191096+RumenaStaneva@users.noreply.github.com>
2021-02-01 14:40:43 +0200

    foundation page finished

--- Foundation-quiz/app.js
+++ Foundation-quiz/app.js
@@ -7,118 +7,119 @@ let namePalette = '';
 
 //nikola's guidance <3
 const products = {
-    makeup: [
-        {
-            name: "CLINIQUE - Dramatically Different Moisturizing Lotion+a ",
-            price: 30,
-            image:"pictures/.png",
+    makeup: [{
+            name: "NARS - All Day Luminous Weightless Foundation",
+            price: 35,
+            image: "pictures/nars-all-day-luminous-weightless-foundation.png",
             id: 0
         },
         {
-            name: 'Marc Jacobs - Beauty Velvet Noir Mascara',
-            price: 27,
-            image: "pictures/marc-jacobs-velvet-noir.png",
+            name: 'HUDA BEAUTY - FauxFilter Luminous Matte Liquid Foundation',
+            price: 34,
+            image: "pictures/HUDA-BEAUTY-fauxfilter.png",
             id: 1
         },
         {
-            name: "Chanel - Le Volume de Chanel Mascara",
-            price: 32,
-            image: "pictures/chanel-le-volume.png", 
+            name: "TOO FACED - Born This Way Foundation( 30ml )",
+            price: 29,
+            image: "pictures/TOO-FACED-Born-This-Way-Foundation.png",
             id: 2
-        }, 
+        },
         {
-            name: "Lancôme - Hypnose Doll Lashes Mascara",
-            price: 28,
-            image: "pictures/lancome-hypnose.png", 
+            name: "IT COSMETICS - Travel Size Your Skin But Better CC+( 12ml )",
+            price: 14,
+            image: "pictures/IT-COSMETICS-Travel-Size-Your-Skin-But-Better.png",
             id: 3
         },
         {
-            name: "Pat McGrath - Labs FetishEyes Mascara",
-            price: 30,
-            image: "pictures/pat-mcgrath.png", 
-            id: 4
-        }, 
-        {
-            name: "Maybelline - Lash Sensational Mascara",
+            name: "NYX PROFESSIONAL MAKEUP - Born To Glow Naturally Radiant Foundation( 30ml )",
             price: 10,
-            image: "pictures/maybelline-lash-sensational.png", 
-            id: 5
-        }, 
+            image: "pictures/NYX-PROFESSIONAL-MAKEUP-Born-To-Glow.png",
+            id: 4
+        },
         {
-            name: "L’Oréal - Voluminous Carbon Black Waterproof Mascara",
-            price: 6,
-            image: "pictures/loreal-voluminous.png", 
+            name: "NYX PROFESSIONAL MAKEUP - Can't Stop Won't Stop 24 Hour Foundation( 30ml )",
+            price: 15,
+            image: "pictures/NYX-PROFESSIONAL-MAKEUP-Can't-Stop-Won't-Stop.png",
+            id: 5
+        },
+        {
+            name: "URBAN DECAY - Stay Naked Foundation( 30ml )",
+            price: 31,
+            image: "pictures/URBAN-DECAY-Stay-Naked-Foundation.png",
             id: 6
         },
         {
-            name: "MAC - Haute and Naughty Lash Mascara",
-            price: 24,
-            image: "pictures/mac-haute-and-naughty.png", 
+            name: "ANASTASIA BEVERLY HILLS - Luminous Foundation( 30ml )",
+            price: 43,
+            image: "pictures/ANASTASIA-BEVERLY-HILLS-Luminous-Foundation.png",
             id: 7
         },
         {
-            name: "IT Cosmetics - Tightline 3-in-1 Black Primer-Eyeliner-Mascara",
-            price: 25,
-            image: "pictures/it-cosmetics.png", 
+            name: "LAURA MERCIER - Flawless Fusion Ultra-Longwear Foundation( 30ml )",
+            price: 36,
+            image: "pictures/LAURA-MERCIER-Flawless-Fusion.png",
             id: 8
         },
         {
-            name: "Giorgio Armani - Beauty Eyes to Kill Mascara",
-            price: 25,
-            image: "pictures/georgio-armani.png", 
+            name: "BEAUTYBLENDER - Bounce( 30ml )",
+            price: 32,
+            image: "pictures/BEAUTYBLENDER-Bounce.png",
             id: 9
         },
         {
-            name: "Benefit - They're Real Mascara",
-            price: 25,
-            image: "pictures/benefit.png", 
+            name: "MILK MAKEUP - Blur Liquid Matte Foundation( 30ml )",
+            price: 36,
+            image: "pictures/MILK-MAKEUP-Blur-Liquid-Matte.png",
             id: 10
         },
         {
-            name: "L'Oréal - Voluminous Lash Paradise Mascara",
-            price: 11,
-            image: "pictures/loreal-paradise.png", 
+            name: "LAURA MERCIER - Silk Crème Oil Free Photo Edition Foundation( 30ml )",
+            price: 36,
+            image: "pictures/LAURA-MERCIER-Silk-Crème.png",
             id: 11
         },
         {
-            name: "Too Faced - Better Than Sex Mascara",
-            price: 23,
-            image: "pictures/better-than-sex.png", 
+            name: "COVER FX - Power Play Foundation( 35ml )",
+            price: 38,
+            image: "pictures/COVER-FX-Power-Play-Foundation.png",
             id: 12
         },
         {
-            name: "Kat Von D - Go Big or Go Home Mascara",
-            price: 20,
-            image: "pictures/kat-von-d.png", 
+            name: "BENEFIT - Hello Happy Flawless Liquid Foundation Mini( 10ml )",
+            price: 13,
+            image: "pictures/COVER-FX-Power-Play-Foundation.png",
             id: 13
         },
-        
+
 
 
     ]
-    
-} 
+
+}
 
 let newId;
 let usedIds = [];
 const numberOfProducts = 14;
+
 function random() {
     newId = Math.floor(Math.random() * numberOfProducts)
     for (let i = 0; i < usedIds.length; i++) {
         let currentId = usedIds[i];
-        if(currentId == newId){
+        if (currentId == newId) {
             random();
             return;
         }
-        
+
     }
     usedIds.push(newId)
-    
+
 }
 random()
 let userInput;
 let btn;
 let whichProduct;
+
 function render() {
     const xProduct = products.makeup.find(x => x.id === newId);
     whichProduct = xProduct;
@@ -132,44 +133,44 @@ function render() {
     <button id="button-submit" type="button">Submit</button>
 </li>`
 
- //btn.removeEventListener('click', trueOrfalse);
- userInput = document.getElementById('input');
- btn = document.getElementById('button-submit');
- btn.addEventListener('click', trueOrfalse);
- userInput.addEventListener("keydown", enter);
+    //btn.removeEventListener('click', trueOrfalse);
+    userInput = document.getElementById('input');
+    btn = document.getElementById('button-submit');
+    btn.addEventListener('click', trueOrfalse);
+    userInput.addEventListener("keydown", enter);
 }
 render()
 
 //const makeup = products.makeup;
 
-  function getUserInput(){
+function getUserInput() {
     return parseInt(userInput.value)
 }
 
 //and here 
-function trueOrfalse(){
+function trueOrfalse() {
     let enteredPrice = getUserInput()
-    if(usedIds.length == numberOfProducts){
-        document.getElementById('winning').style.display='block';
-        document.getElementById('restart').addEventListener('click', function() {
+    if (usedIds.length == numberOfProducts) {
+        document.getElementById('winning').style.display = 'block';
+        document.getElementById('restart').addEventListener('click', function () {
             usedIds = [];
-            document.getElementById('winning').style.display='none';
+            document.getElementById('winning').style.display = 'none';
             random();
             render();
         })
         return;
-    }else if (whichProduct.price == enteredPrice) {
-        document.getElementById('correct').style.display='block';
+    } else if (whichProduct.price == enteredPrice) {
+        document.getElementById('correct').style.display = 'block';
         random();
         render();
-    
+
     } else {
-        document.getElementById('wrong').style.display='block';
-        
+        document.getElementById('wrong').style.display = 'block';
+
     }
-    
-    
-    
+
+
+
 }
 
 
@@ -177,16 +178,16 @@ function trueOrfalse(){
 
 //ask Nikola
 for (let i = 0; i < closebtn.length; i++) {
-    closebtn[i].addEventListener("click", function() {
-      this.parentElement.style.display = 'none';
+    closebtn[i].addEventListener("click", function () {
+        this.parentElement.style.display = 'none';
     });
-  }
-  
+}
+
 //when pushed enter
 
 function enter(pushed) {
     if (pushed.keyCode === 13) {
-    pushed.preventDefault();
-    btn.click();
+        pushed.preventDefault();
+        btn.click();
     }
-  }
+}
